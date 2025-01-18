d2bb03ae552ba33e11b22b2fb0e7bace4dd3f550
Erik Fortune <ErikFortune@outlook.com>
2025-01-18 02:12:58 -0800

    checkpoint added unit tests

---
+++ libraries/ts-res/src/test/unit/common/conditions.test.ts
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2025 Erik Fortune
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import * as TsRes from '../../..';
+
+const validIdentifiers: string[] = [
+  'abc',
+  '_a10',
+  'this-is-an-identifier',
+  '_This_Is_Also-An_Identifier10',
+  'A'
+];
+
+const invalidIdentifiers: string[] = [
+  '',
+  ' not_an_identifier',
+  'also not an identifier',
+  '1not_identifier',
+  'rats!'
+];
+
+describe('common conditions', () => {
+  test.each(validIdentifiers)('%s is valid qualifierName', (identifier) => {
+    expect(TsRes.Common.Conditions.isValidQualifierName(identifier)).toBe(true);
+  });
+  test.each(invalidIdentifiers)('%s is not a valid qualifierName', (identifier) => {
+    expect(TsRes.Common.Conditions.isValidQualifierName(identifier)).toBe(false);
+  });
+});
