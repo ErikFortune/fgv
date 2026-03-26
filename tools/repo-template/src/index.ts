5d2ffcfa579db173cea819e6526e6941009d0d99
Erik Fortune <ErikFortune@outlook.com>
2026-03-26 16:06:09 +0100

    Add init-library command to @fgv/repo-template (#258)
    * Add init-library command to @fgv/repo-template CLI
    
    Scaffolds a new library package within an existing Rush monorepo:
    - Creates package.json, tsconfig.json, config/rig.json, jest config, src/index.ts
    - Includes @fgv/ts-utils and @fgv/ts-json-base as default dependencies
    - Includes @fgv/ts-utils-jest as a default dev dependency
    - --rig option: dual (default), node, or browser
    - --category option: libraries (default), tools, apps, services
    - --fgv-dep-version: "workspace:*" (default, for fgv) or version range for consumers
    - Registers the new project in rush.json via JSONC patching
    
    Tested: scaffolded library builds in both fgv (workspace:*) and
    consumer repo (^5.1.0-0) contexts.

--- tools/repo-template/src/index.ts
+++ tools/repo-template/src/index.ts
@@ -8,6 +8,7 @@ export { RepoTemplateCli } from './cli';
 export { runCreate, ICreateOptions } from './commands/create';
 export { runSync, ISyncOptions } from './commands/sync';
 export { runPatch, IPatchOptions, parsePatchArgs } from './commands/patch';
+export { runInitLibrary, IInitLibraryOptions, RigType, CategoryType } from './commands/init-library';
 export { applyOperations, applyOperation, patchFile, IPatchOperation } from './packlets/jsonc';
 export { loadManifest, IManifest, ISharedFile, ISharedPackage, ITemplatedFile } from './packlets/manifest';
 export { renderTemplate, renderTemplateFile, ITemplateVars } from './packlets/template';
