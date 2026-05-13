6576c9c4a90c7e5be8d866c4e98c3d7930248e47
Erik Fortune <ErikFortune@outlook.com>
2026-05-13 02:20:39 -0700

    rename

--- libraries/ts-utils/src/packlets/base/utils.ts
+++ libraries/ts-utils/src/packlets/base/utils.ts
@@ -27,14 +27,14 @@ import { Result, fail, succeed } from './result';
  * If T is not an array/tuple, results in `never`.
  * @public
  */
-export type AnyValueOf<T> = T extends readonly unknown[] ? T[number] : never;
+export type OneOf<T> = T extends readonly unknown[] ? T[number] : never;
 
 /**
  * Union of all string values in an array/tuple type, preserving literal types if possible.
  * If T is not an array/tuple, results in `never`.
  * @public
  */
-export type StringValueOf<T> = Extract<AnyValueOf<T>, string>;
+export type StringOneOf<T> = Extract<OneOf<T>, string>;
 
 /**
  * Helper type-guard function to report whether a specified key is present in
